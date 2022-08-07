8ebd9c960aa3e490c17ea9e4df0f3671025f6777
Mohamed-gaal <100410832+Mohamed-gaal@users.noreply.github.com>
2022-08-07 20:15:57 +0300

    My newly added function

--- my_project.py
+++ my_project.py
@@ -1,3 +1,10 @@
 # Lets test the final Version of The project 
 
+import math
+def NewFun(df):
+    'retunrs maximum deviation * sqrt(2)'
+    p= df.iloc[:,3].abs().max()
+    p= p * math.sqrt(2)
+    return p
+
 
