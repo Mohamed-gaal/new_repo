045543bd4a58a18d66da947bf51004ef53e5068a
Mohamed-gaal <100410832+Mohamed-gaal@users.noreply.github.com>
2022-10-19 13:16:24 +0300

    I added a new function that counts the total mapped test points

--- my_project.py
+++ my_project.py
@@ -6,3 +6,11 @@ def NewFun(df):
     p= df.iloc[:,3].abs().max()
     p= p * math.sqrt(2)
     return p
+
+def Mapped_points_total(self):
+    'take the mapped test points and dataframe and returns the dimensions'
+    if len(self.index) >100:
+        print('Review your test points')
+    else: 
+        print('Your test points total is {}'.format(len(self.index)))
+    
