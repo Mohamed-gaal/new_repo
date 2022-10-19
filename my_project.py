c5039707b59e7a809c8cdfb2b17fa09a8d1f8978
Mohamed-gaal <100410832+Mohamed-gaal@users.noreply.github.com>
2022-10-19 14:37:29 +0300

    Update my_project.py

--- my_project.py
+++ my_project.py
@@ -7,10 +7,5 @@ def NewFun(df):
     p= p * math.sqrt(2)
     return p
 
-def Mapped_points_total(self):
-    'take the mapped test points and dataframe and returns the dimensions'
-    if len(self.index) >100:
-        print('Review your test points')
-    else: 
-        print('Your test points total is {}'.format(len(self.index)))
+
     
